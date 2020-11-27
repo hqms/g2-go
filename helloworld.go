cb9b4ebc244e990f170fa80e92006f0f6e785ad3
HQM <hakim@femaledaily.com>
2020-11-27 10:48:52 +0700

    say your name

--- helloworld.go
+++ helloworld.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main(){
-  fmt.Println("Hello World")
+  var name string
+
+  fmt.Print("What's your name? ")
+  fmt.Scanln(&name)
+  fmt.Println("Hello ", name)
 }
