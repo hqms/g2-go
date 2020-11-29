761f0428f237a3dcc85fc82b86c6f047027b0419
HQM <hakim@femaledaily.com>
2020-11-29 15:05:36 +0700

    add endpoint

--- gokit/main.go
+++ gokit/main.go
@@ -1,7 +1,9 @@
 package gokit
 
 import (
+	"context"
 	"errors"
+	"github.com/go-kit/kit/endpoint"
 	"strings"
 )
 
@@ -43,4 +45,25 @@ type countRequest struct {
 
 type countResponse struct {
 	V int `json:"v"`
+}
+
+type Endpoint func(ctx context.Context, request interface{}) (response interface{}, err error)
+
+func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(uppercaseRequest)
+		v, err := svc.Uppercase(req.S)
+		if err != nil {
+			return uppercaseResponse{v, err.Error()}, nil
+		}
+		return uppercaseResponse{v, ""}, nil
+	}
+}
+
+func makeCountEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(countRequest)
+		v := svc.Count(req.S)
+		return countResponse{v}, nil
+	}
 }
