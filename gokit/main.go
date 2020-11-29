5bc392cc68191dfd7163a13e92db0aa162b12b11
HQM <hakim@femaledaily.com>
2020-11-29 15:09:17 +0700

    expose the service in the main function

--- gokit/main.go
+++ gokit/main.go
@@ -2,8 +2,12 @@ package gokit
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+	"log"
+	"net/http"
 	"strings"
 )
 
@@ -66,4 +70,43 @@ func makeCountEndpoint(svc StringService) endpoint.Endpoint {
 		v := svc.Count(req.S)
 		return countResponse{v}, nil
 	}
+}
+func main() {
+	svc := stringService{}
+
+	uppercaseHandler := httptransport.NewServer(
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
+		encodeResponse,
+	)
+
+	countHandler := httptransport.NewServer(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/uppercase", uppercaseHandler)
+	http.Handle("/count", countHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func decodeUppercaseRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request uppercaseRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func decodeCountRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request countRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
 }
