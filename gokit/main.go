17ab7eb58b64ddf563dff57d337e828446acaa8d
HQM <hakim@femaledaily.com>
2020-11-29 14:03:31 +0700

    define request and response as struct

--- gokit/main.go
+++ gokit/main.go
@@ -27,3 +27,20 @@ func (stringService) Count(s string) int {
 
 // ErrEmpty is returned when input string is empty
 var ErrEmpty = errors.New("Empty string")
+
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type uppercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't JSON-marshal, so we use a string
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
