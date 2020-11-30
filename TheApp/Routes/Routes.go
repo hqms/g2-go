074fc2273fd5a7ea546f17e360d5a7144e37bf27
HQM <hakim@femaledaily.com>
2020-12-01 06:25:44 +0700

    routers

--- TheApp/Routes/Routes.go
+++ TheApp/Routes/Routes.go
@@ -1,1 +1,17 @@
 package Routes
+
+import (
+
+	"github.com/gin-gonic/gin"
+	"theapp/Controllers"
+)
+
+func SetupRouter() *gin.Engine  {
+	r := gin.Default()
+
+	usergroup := r.Group("/userapi")
+	{
+		usergroup.GET("user", Controllers.GetUsers)
+	}
+	return r
+}
