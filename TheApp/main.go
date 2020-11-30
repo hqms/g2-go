10e0d527f56a9386cd19b2ec99ac0bdaa5980ecb
HQM <hakim@femaledaily.com>
2020-12-01 06:25:58 +0700

    main module

--- TheApp/main.go
+++ TheApp/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"theapp/Config"
+	"theapp/Models"
+	"theapp/Routes"
+)
+
 func main() {
-	
+	var err error
+	Config.DB, err = gorm.Open(sqlite.Open(Config.DbUrl(Config.BuildDBConfig())), &gorm.Config{})
+	if err != nil{
+		panic("Cannot connect to DB")
+	}
+
+	//defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+
+	r := Routes.SetupRouter()
+	r.Run()
 }
