06e144ea5bb8f77af82308d333e9a90b90890ec3
HQM <hakim@femaledaily.com>
2020-12-01 01:30:49 +0700

    simple condition

--- gorm/read.go
+++ gorm/read.go
@@ -7,10 +7,11 @@ import (
 )
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("/Users/nurhakimarif/workspaces/g2go/gorm/test.db"), &gorm.Config{})
 	if err != nil {
 		panic("Failed connect to DB")
 	}
+
 	var user User
 
 	db.First(&user)
@@ -18,9 +19,26 @@ func main() {
 
 	result := map[string]interface{}{}
 	db.Model(&User{}).First(result)
-	fmt.Println(result)
+	fmt.Println(result["name"])
 
 	result = map[string]interface{}{}
 	db.Table("users").Take(&result)
 	fmt.Println(result)
+
+	var users User
+	all,_ := db.Model(&users).Rows()
+
+	for all.Next(){
+		db.ScanRows(all, &users)
+		fmt.Println(users.ID)
+	}
+
+	var u User
+	where,_ := db.Table("users").Where("age=?", 17).Rows()
+
+	for where.Next(){
+		db.ScanRows(where, &u)
+		fmt.Println(u.ID)
+	}
+
 }
