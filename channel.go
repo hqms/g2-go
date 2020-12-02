ba511f24d8334b97ee6d139c7877c61675345eb7
HQM <hakim@femaledaily.com>
2020-12-03 04:31:18 +0700

    multiple channel

--- channel.go
+++ channel.go
@@ -4,13 +4,32 @@ import (
 	"fmt"
 )
 
-func hello(done chan bool) {
-	fmt.Println("Hello world goroutine")
-	done <- true
+func calcSquares(number int, squareop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit
+		number /= 10
+	}
+	squareop <- sum
 }
+
+func calcCubes(number int, cubeop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit * digit
+		number /= 10
+	}
+	cubeop <- sum
+}
+
 func main() {
-	done := make(chan bool)
-	go hello(done)
-	<-done
-	fmt.Println("main function")
+	number := 589
+	sqrch := make(chan int)
+	cubech := make(chan int)
+	go calcSquares(number, sqrch)
+	go calcCubes(number, cubech)
+	squares, cubes := <-sqrch, <-cubech
+	fmt.Println("Final output", squares + cubes)
 }
