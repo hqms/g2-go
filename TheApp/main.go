5dacb88c6145f322a645f987c6b99e54567e14f9
HQM <hakim@femaledaily.com>
2020-12-02 08:47:38 +0700

    add sentry

--- TheApp/main.go
+++ TheApp/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"github.com/getsentry/sentry-go"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"log"
 	"theapp/Config"
 	"theapp/Models"
 	"theapp/Routes"
+	"time"
 )
+
 
 func main() {
 	var err error
@@ -18,6 +22,14 @@ func main() {
 	//defer Config.DB.Close()
 	Config.DB.AutoMigrate(&Models.User{})
 
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://52b4560b8c1046a8abdbafbdad51d369@o485449.ingest.sentry.io/5540851",
+	});  err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+
+	defer sentry.Flush(2 * time.Second)
+	
 	r := Routes.SetupRouter()
 
 	r.Run()
