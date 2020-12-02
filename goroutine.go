304b211fc7163cc8a57be719ccec624c73317ecc
HQM <hakim@femaledaily.com>
2020-12-03 03:58:05 +0700

    multiple goroutine

--- goroutine.go
+++ goroutine.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-"fmt"
+	"fmt"
+	"time"
 )
 
-func hello() {
-	fmt.Println("Hello world goroutine")
+func numbers() {
+	for i := 1; i <= 5; i++ {
+		time.Sleep(250 * time.Millisecond)
+		fmt.Printf("%d ", i)
+	}
+}
+func alphabets() {
+	for i := 'a'; i <= 'e'; i++ {
+		time.Sleep(400 * time.Millisecond)
+		fmt.Printf("%c ", i)
+	}
 }
 func main() {
-	go hello()
-	fmt.Println("main function")
+	go numbers()
+	go alphabets()
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println("main terminated")
 }
