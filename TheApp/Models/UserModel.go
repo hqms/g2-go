ce6f5b3b0dc712b29b667132b2e882bde769e3e9
HQM <hakim@femaledaily.com>
2020-12-02 07:14:38 +0700

    Bind hook on every data access

--- TheApp/Models/UserModel.go
+++ TheApp/Models/UserModel.go
@@ -1,5 +1,10 @@
 package Models
 
+import (
+	"gorm.io/gorm"
+	"theapp/Service"
+)
+
 type User struct {
 	Id      uint   `json:"id"`
 	Name    string `json:"name"`
@@ -9,4 +14,19 @@ type User struct {
 }
 func (b *User) TableName() string {
 	return "user"
+}
+
+func (u *User)BeforeCreate(tx *gorm.DB)  (err error){
+	u.Name = Service.Encrypt(u.Name)
+	return
+}
+
+func (u *User)BeforeUpdate(tx *gorm.DB)  (err error){
+	u.Name = Service.Encrypt(u.Name)
+	return
+}
+
+func (u *User) AfterFind(tx *gorm.DB) (err error) {
+	u.Name = Service.Decrypt(u.Name)
+	return 
 }
